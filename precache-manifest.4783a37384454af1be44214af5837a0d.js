3613fb70e80a7c0ee38a2e502633f7768e5c201f
David A <david1906@outlook.com>
2020-02-12 00:03:52 -0600

    Updates

---
+++ precache-manifest.4783a37384454af1be44214af5837a0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc0e9bb9539e31151523a34b0794cdf5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "983d7152bd7988452e49",
+    "url": "/portfolio/static/css/main.bb7b1075.chunk.css"
+  },
+  {
+    "revision": "3e2d9b125709fc730d3e",
+    "url": "/portfolio/static/js/2.b417dfd0.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio/static/js/2.b417dfd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "983d7152bd7988452e49",
+    "url": "/portfolio/static/js/main.18837a5b.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
